f41f954e6190cbb3381ff3536e6324c574654872
Peter Kreppein <pmkreppein@gmail.com>
2019-01-04 22:53:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = "Pete"
+const leastFavoriteCustomer = "bob?"
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+  bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob"
+}
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "kayla"
+}
